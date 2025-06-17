876784afe8c88559a62d63ec206c77ce37af7f49
Muhammad Junaid Raza <junaidnaveed030000@gmail.com>
2025-06-18 02:24:06 +0500

    fix the previous bugs

---
+++ app/_layout.jsx
@@ -0,0 +1,25 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#ff8c00",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+          fontSize: 20,
+        },
+        contentStyle: {
+          backgroundColor: "#fff",
+          paddingHorizontal: 10,
+          paddingTop: 10,
+        },
+      }}
+    >
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+    </Stack>
+  );
+}
---
+++ app/notes/index.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+const NoteScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Notes</Text>
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#fff",
+  },
+})
+export default NoteScreen
